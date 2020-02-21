74016b9bc1b33569d898d431e552e5211f486cef
Saunak Sahoo <saunak.sahoo@aciworldwide.com>
2020-02-21 10:43:49 +0530

    Initial commit

---
+++ src/main/java/com/aciworldwide/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.aciworldwide.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home")
+	public String viewHomePage() {
+		return "home";
+	}
+
+}
